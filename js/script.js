5e5eed43ca8b160fbe9b930938db7f9eb210495d
kaushik100b <kaushik100b@gmail.com>
2025-06-15 17:29:23 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,6 +8,36 @@ window.addEventListener("scroll", function () {
   });
 
 
+  
+  $(document).ready(function(){
+    $(".partner-carousel").owlCarousel({
+      loop: true,
+      margin: 30,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 3
+        },
+        768: {
+          items: 4
+        },
+        992: {
+          items: 5  // ✅ 5 items in one line on larger screens
+        },
+        1200: {
+          items: 5
+        }
+      }
+    });
+  });
+
 $(document).ready(function(){
   var owl = $(".owl-carousel");
   owl.owlCarousel({
